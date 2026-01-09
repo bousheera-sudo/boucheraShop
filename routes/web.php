5f09eacb25f4379486998ab7e723637385d87752
bouchra baifouch <bouchrabaidouch@gmail.com>
2026-01-09 11:58:09 +0100

    llll

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,36 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
+/* Page d'accueil */
 Route::get('/', function () {
-    return view('welcome');
+    return view('Home');
+});
+
+/* Route dynamique produits */
+Route::get('/produits/{cat}', function ($cat) {
+
+    $produits = [];
+
+    if ($cat == 'hicking') {
+        $produits = [
+            ["nom" => "Sac à dos", "prix" => 200, "image" => "sac_do.jfif"],
+            ["nom" => "Tente", "prix" => 300, "image" => "tent.jfif"],
+            ["nom" => "Montre GPS", "prix" => 150, "image" => "watch_gps.jfif"],
+        ];
+    }
+    elseif ($cat == 'electromenager') {
+        $produits = [
+            ["nom" => "Machine à laver", "prix" => 3000, "image" => "machine_lav.jfif"],
+            ["nom" => "Four", "prix" => 1500, "image" => "four.jfif"],
+            ["nom" => "Micro-onde", "prix" => 1000, "image" => "micro_onde.jfif"],
+        ];
+    }
+    else {
+        abort(404);
+    }
+
+    return view('Produits', [
+        'products' => $produits,
+        'categorie' => $cat
+    ]);
 });
